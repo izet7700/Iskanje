9b74ec120045342e6c9ca2a04674eb7c8019db08
izet7700 <43537592+izet7700@users.noreply.github.com>
2019-04-06 20:25:02 +0200

    Initial commit

---
+++ Iskanje/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Iskanje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vnosTextBox = new System.Windows.Forms.TextBox();
+            this.dodajButton = new System.Windows.Forms.Button();
+            this.vsebujeButton = new System.Windows.Forms.Button();
+            this.minButton = new System.Windows.Forms.Button();
+            this.maxButton = new System.Windows.Forms.Button();
+            this.povpButton = new System.Windows.Forms.Button();
+            this.vsotaButton = new System.Windows.Forms.Button();
+            this.vsBinarButton = new System.Windows.Forms.Button();
+            this.urediIzbButton = new System.Windows.Forms.Button();
+            this.urediMehButton = new System.Windows.Forms.Button();
+            this.urediVstButton = new System.Windows.Forms.Button();
+            this.hitUredButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // vnosTextBox
+            // 
+            this.vnosTextBox.Location = new System.Drawing.Point(13, 19);
+            this.vnosTextBox.Name = "vnosTextBox";
+            this.vnosTextBox.Size = new System.Drawing.Size(169, 20);
+            this.vnosTextBox.TabIndex = 0;
+            // 
+            // dodajButton
+            // 
+            this.dodajButton.Location = new System.Drawing.Point(206, 11);
+            this.dodajButton.Name = "dodajButton";
+            this.dodajButton.Size = new System.Drawing.Size(75, 35);
+            this.dodajButton.TabIndex = 1;
+            this.dodajButton.Text = "Dodaj";
+            this.dodajButton.UseVisualStyleBackColor = true;
+            // 
+            // vsebujeButton
+            // 
+            this.vsebujeButton.Location = new System.Drawing.Point(287, 11);
+            this.vsebujeButton.Name = "vsebujeButton";
+            this.vsebujeButton.Size = new System.Drawing.Size(75, 35);
+            this.vsebujeButton.TabIndex = 2;
+            this.vsebujeButton.Text = "Vsebuje";
+            this.vsebujeButton.UseVisualStyleBackColor = true;
+            // 
+            // minButton
+            // 
+            this.minButton.Location = new System.Drawing.Point(13, 49);
+            this.minButton.Name = "minButton";
+            this.minButton.Size = new System.Drawing.Size(75, 41);
+            this.minButton.TabIndex = 3;
+            this.minButton.Text = "Min";
+            this.minButton.UseVisualStyleBackColor = true;
+            // 
+            // maxButton
+            // 
+            this.maxButton.Location = new System.Drawing.Point(107, 49);
+            this.maxButton.Name = "maxButton";
+            this.maxButton.Size = new System.Drawing.Size(75, 41);
+            this.maxButton.TabIndex = 4;
+            this.maxButton.Text = "Max";
+            this.maxButton.UseVisualStyleBackColor = true;
+            // 
+            // povpButton
+            // 
+            this.povpButton.Location = new System.Drawing.Point(107, 105);
+            this.povpButton.Name = "povpButton";
+            this.povpButton.Size = new System.Drawing.Size(75, 41);
+            this.povpButton.TabIndex = 6;
+            this.povpButton.Text = "Povprečje";
+            this.povpButton.UseVisualStyleBackColor = true;
+            // 
+            // vsotaButton
+            // 
+            this.vsotaButton.Location = new System.Drawing.Point(13, 105);
+            this.vsotaButton.Name = "vsotaButton";
+            this.vsotaButton.Size = new System.Drawing.Size(75, 41);
+            this.vsotaButton.TabIndex = 5;
+            this.vsotaButton.Text = "Vsota";
+            this.vsotaButton.UseVisualStyleBackColor = true;
+            // 
+            // vsBinarButton
+            // 
+            this.vsBinarButton.Location = new System.Drawing.Point(371, 11);
+            this.vsBinarButton.Name = "vsBinarButton";
+            this.vsBinarButton.Size = new System.Drawing.Size(75, 56);
+            this.vsBinarButton.TabIndex = 7;
+            this.vsBinarButton.Text = "Vsebuje binarno";
+            this.vsBinarButton.UseVisualStyleBackColor = true;
+            // 
+            // urediIzbButton
+            // 
+            this.urediIzbButton.Location = new System.Drawing.Point(371, 73);
+            this.urediIzbButton.Name = "urediIzbButton";
+            this.urediIzbButton.Size = new System.Drawing.Size(75, 56);
+            this.urediIzbButton.TabIndex = 8;
+            this.urediIzbButton.Text = "Uredi z izbiranjem";
+            this.urediIzbButton.UseVisualStyleBackColor = true;
+            // 
+            // urediMehButton
+            // 
+            this.urediMehButton.Location = new System.Drawing.Point(206, 73);
+            this.urediMehButton.Name = "urediMehButton";
+            this.urediMehButton.Size = new System.Drawing.Size(75, 56);
+            this.urediMehButton.TabIndex = 9;
+            this.urediMehButton.Text = "Uredi z mehurčki";
+            this.urediMehButton.UseVisualStyleBackColor = true;
+            // 
+            // urediVstButton
+            // 
+            this.urediVstButton.Location = new System.Drawing.Point(287, 73);
+            this.urediVstButton.Name = "urediVstButton";
+            this.urediVstButton.Size = new System.Drawing.Size(75, 56);
+            this.urediVstButton.TabIndex = 11;
+            this.urediVstButton.Text = "Uredi z vstavljanjem";
+            this.urediVstButton.UseVisualStyleBackColor = true;
+            // 
+            // hitUredButton
+            // 
+            this.hitUredButton.Location = new System.Drawing.Point(206, 135);
+            this.hitUredButton.Name = "hitUredButton";
+            this.hitUredButton.Size = new System.Drawing.Size(240, 36);
+            this.hitUredButton.TabIndex = 12;
+            this.hitUredButton.Text = "Hitno uredi";
+            this.hitUredButton.UseVisualStyleBackColor = true;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(13, 153);
+            this.outputLabel.MaximumSize = new System.Drawing.Size(170, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.TabIndex = 13;
+            this.outputLabel.Text = "label1\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.hitUredButton);
+            this.Controls.Add(this.urediVstButton);
+            this.Controls.Add(this.urediMehButton);
+            this.Controls.Add(this.urediIzbButton);
+            this.Controls.Add(this.vsBinarButton);
+            this.Controls.Add(this.povpButton);
+            this.Controls.Add(this.vsotaButton);
+            this.Controls.Add(this.maxButton);
+            this.Controls.Add(this.minButton);
+            this.Controls.Add(this.vsebujeButton);
+            this.Controls.Add(this.dodajButton);
+            this.Controls.Add(this.vnosTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox vnosTextBox;
+        private System.Windows.Forms.Button dodajButton;
+        private System.Windows.Forms.Button vsebujeButton;
+        private System.Windows.Forms.Button minButton;
+        private System.Windows.Forms.Button maxButton;
+        private System.Windows.Forms.Button povpButton;
+        private System.Windows.Forms.Button vsotaButton;
+        private System.Windows.Forms.Button vsBinarButton;
+        private System.Windows.Forms.Button urediIzbButton;
+        private System.Windows.Forms.Button urediMehButton;
+        private System.Windows.Forms.Button urediVstButton;
+        private System.Windows.Forms.Button hitUredButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Iskanje/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iskanje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Tab
+    {
+        private int[] Tabela = new int[100];
+        public int Dolzina { get; set; }
+        public Tab()
+        {
+
+        }
+        public void Dodaj(int x)
+        {
+            Dolzina++;
+            Tabela[Dolzina] = x;
+        }
+        public bool Vsebuje(int x)
+        {
+            bool vs = false;
+            for (int i = 0; i <= Dolzina; i++)
+            {
+                if (x == Tabela[i])
+                {
+                    vs = true;
+                    break;
+                }
+            }
+            return vs;
+        }
+        public override string ToString()
+        {
+            string tab="";
+            for (int i = 0; i <= Dolzina; i++)
+            {
+                tab = Tabela[i] + ", ";
+            }
+            return tab;
+        }
+        public int Vsota()
+        {
+            int vs = 0;
+            for (int i = 0; i <= Dolzina; i++)
+            {
+                vs += Tabela[i];
+            }
+            return vs;
+        }
+        public int Min()
+        {
+            int min = Tabela[0];
+            for (int i = 0; i <= Dolzina; i++)
+            {
+                if (Tabela[i] < min) min = Tabela[i];
+            }
+            return min;
+        }
+        public int Max()
+        {
+            int max = Tabela[0];
+            for (int i = 0; i <= Dolzina; i++)
+            {
+                if (Tabela[i] > max) max = Tabela[i];
+            }
+            return max;
+        }
+        public double Povprecje()
+        {
+            return Vsota() / Dolzina;
+        }
+        public bool VsebujeBinarno(int x)
+        {
+            bool binVs = false;
+            int min = 0, max = Dolzina;
+            do
+            {
+                int mid = (min + max) / 2;
+                if (x > Tabela[mid]) min = mid + 1;
+                else if (x < Tabela[mid]) max = mid - 1;
+                else if (x == Tabela[mid]) binVs = true;
+            } while (min <= max);
+            return binVs;
+        }
+    }
+}
